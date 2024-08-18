923cb76dd9529c5691775656c8a29184e0ad3fa0
OlhaMasl <maslakok@gmail.com>
2024-08-18 10:17:05 +0300

    add redux-persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,11 @@
-import { useEffect, useState } from "react";
+
 import "../index.css";
-import initialContacts from "../contacts.json";
 import ContactList from "./ContactList/ContactList";
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 
 
 const App = () => {
-
-//   const [contacts, setContacts] = useState(() => {
-//     const savedContacts = JSON.parse(window.localStorage.getItem("contacts"));
-//     if (savedContacts !== null) {
-//       return savedContacts;
-//     };
-//     return initialContacts;
-//   });
-  
-//   const [filter, setFilter] = useState("");
-  
-//   useEffect(() => {
-//     window.localStorage.setItem("contacts", JSON.stringify(contacts));
-//   }, [contacts]);
-
-//  const addContact = (newContact) => {
-//     setContacts((prevContacts) => {
-//       return [...prevContacts, newContact];
-//     });
-//   };
-
-//   const deleteContact = (contactId) => {
-//    setContacts((prevContacts) => {
-//      return prevContacts.filter((contact) => contact.id !== contactId);
-//     });
-//   };
-//   const filteredContacts = contacts.filter((contact) => contact.name.toLowerCase().includes(filter.toLowerCase()));
 
     return (
       <div className="container">
